9d0a3a4fab0d673660efb5f65ffcd9aeb2c358be
Gilbert Gilb's <gilbsgilbert@gmail.com>
2020-10-08 11:25:13 +0200

    fix tests isolation (key is not supported for files lock)

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"testing"
+	"time"
 
 	cmd "github.com/gilbsgilbs/glloq/cmd"
 	"github.com/stretchr/testify/assert"
@@ -20,21 +21,26 @@ func TestCli(t *testing.T) {
 	})
 
 	t.Run("runs concurrent run", func(t *testing.T) {
-		key := t.Name()
+		done := false
 		go func() {
-			if _, err := cmd.RunGlloq(
-				append(env, "GLLOQ_KEY="+key),
-				[]string{"sleep", "10"},
-			); err != nil {
+			if _, err := cmd.RunGlloq(env, []string{"sleep", ".3"}); err != nil {
 				panic(err)
 			}
+			done = true
 		}()
 
-		exitCode, err := cmd.RunGlloq(
-			append(env, "GLLOQ_KEY="+key),
-			[]string{"sleep", "1"},
-		)
+		// Wait a bit so that the routine can start
+		time.Sleep(50 * time.Millisecond)
 
+		// The routine should be running, not done yet.
+		assert.False(t, done)
+
+		exitCode, err := cmd.RunGlloq(env, []string{"echo", "ok"})
+
+		// The command should have waited for the routine to finish.
+		assert.True(t, done)
+
+		// Check the command exit code.
 		assert.Equal(t, 0, exitCode)
 		assert.Nil(t, err)
 	})
